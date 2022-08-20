23fc67fe6700443719220afd149bb3940b7018d0
Jaydenkim4524 <108809241+Jaydenkim4524@users.noreply.github.com>
2022-08-20 14:03:25 +0900

    Create if_practice.c

---
+++ if_practice.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+  int a, b;
+  scanf("%d %d", &a, &b);
+  if (a > b) {
+    if (a % b == 0) {
+      printf("나머지는 0입니다\n");
+    } else {
+      printf("나머지는 0이 아닙니다\n");
+    }
+
+  } else {
+    if (b % a == 0) {
+      printf("나머지는 0입니다\n");
+    } else {
+      printf("나머지는 0이 아닙니다\n");
+    }
+}
